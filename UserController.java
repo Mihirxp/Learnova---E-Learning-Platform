3cbce5029297ac65050e9b0ad9fd515a131b0538
Mihirxp <palkarmihir21@gmail.com>
2025-10-04 11:04:17 +0530

    UserController.java

---
+++ UserController.java
@@ -0,0 +1,21 @@
+package com.learnova.controller;
+
+import com.learnova.entity.User;
+import com.learnova.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User savedUser = userService.registerUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+}
